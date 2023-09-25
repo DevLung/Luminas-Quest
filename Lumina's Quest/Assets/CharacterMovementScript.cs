b8f5d43c356dc29a5aaae154d37366e12efc95c6
DevLung <immo.friedhoff@web.de>
2023-09-25 20:00:11 +0200

    add double jump

--- Lumina's Quest/Assets/CharacterMovementScript.cs
+++ Lumina's Quest/Assets/CharacterMovementScript.cs
@@ -10,6 +10,8 @@ public class CharacterMovementScript : MonoBehaviour
     public CapsuleCollider2D capsuleCollider;
     public float moveSpeed;
     public float jumpHeight;
+    public bool doubleJump;
+    private int jumpCount;
     private PlayerInputActions playerInput;
     private Collision2D lastCollision;
 
@@ -34,14 +36,24 @@ public class CharacterMovementScript : MonoBehaviour
 
     private void Jump(InputAction.CallbackContext context)
     {
-        // on jump input while on floor
-        if (capsuleCollider.IsTouching(lastCollision.collider) && lastCollision.collider.tag == "floor")
+        // on jump input while on floor or after first jump if double jump is enabled
+        if (jumpCount == 1 || capsuleCollider.IsTouching(lastCollision.collider) && lastCollision.collider.tag == "floor")
         {
+            // calculate height of character feet and top surface of floor and only allow jump if the character feet are above the top surface of floor
             float feetHeight = capsuleCollider.transform.position.y - capsuleCollider.bounds.extents.y;
             float FloorHeight = lastCollision.collider.transform.position.y + lastCollision.collider.bounds.extents.y;
-            if (feetHeight >= FloorHeight)
+            if (jumpCount == 1 || feetHeight >= FloorHeight)
             {
                 rigidBody.AddForce(Vector3.up * jumpHeight, ForceMode2D.Impulse);
+                // increment jump count if double jump is enabled and reset if it is higher than 1 so you can only jump in the air once 
+                if (doubleJump)
+                {
+                    jumpCount++;
+                    if (jumpCount > 1)
+                    {
+                        jumpCount = 0;
+                    }
+                }
             }
         }
     }
