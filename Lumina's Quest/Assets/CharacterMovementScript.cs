14b5e0f45701a90df242f41a62c52e80b86811c1
DevLung <immo.friedhoff@web.de>
2023-09-30 16:31:47 +0200

    changes on camera and character
    - change camera size from 200 to 50
    - change character collider to polygon collider
    
    pls scale up sprite

--- Lumina's Quest/Assets/CharacterMovementScript.cs
+++ Lumina's Quest/Assets/CharacterMovementScript.cs
@@ -7,7 +7,7 @@ using static UnityEditor.Timeline.TimelinePlaybackControls;
 public class CharacterMovementScript : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
-    public CapsuleCollider2D capsuleCollider;
+    public PolygonCollider2D poligonCollider;
     public float moveSpeed;
     public float jumpHeight;
     public bool doubleJump;
@@ -22,6 +22,7 @@ public class CharacterMovementScript : MonoBehaviour
         playerInput.Movement.jump.performed += Jump;
     }
 
+
     private void Update()
     {
         // on move input
@@ -29,37 +30,39 @@ public class CharacterMovementScript : MonoBehaviour
         rigidBody.transform.position += new Vector3(inputVector.x, 0, 0) * moveSpeed * Time.deltaTime;
     }
 
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         lastCollision = collision;
     }
 
+    
     private void Jump(InputAction.CallbackContext context)
     {
-        // on jump input while on floor or after first jump if double jump is enabled
-        if (jumpCount == 1 || capsuleCollider.IsTouching(lastCollision.collider) && lastCollision.collider.tag == "floor")
+        // calculate height of character feet and top surface of floor
+        float feetHeight = poligonCollider.transform.position.y - poligonCollider.bounds.extents.y;
+        float FloorHeight = lastCollision.collider.transform.position.y + lastCollision.collider.bounds.extents.y;
+
+        // on jump input while on floor, only if the character feet are above the top surface of floor or after first jump if double jump is enabled
+        if (jumpCount == 1 || poligonCollider.IsTouching(lastCollision.collider) && lastCollision.collider.tag == "floor" && feetHeight >= FloorHeight)
         {
-            // calculate height of character feet and top surface of floor and only allow jump if the character feet are above the top surface of floor
-            float feetHeight = capsuleCollider.transform.position.y - capsuleCollider.bounds.extents.y;
-            float FloorHeight = lastCollision.collider.transform.position.y + lastCollision.collider.bounds.extents.y;
-            if (jumpCount == 1 || feetHeight >= FloorHeight)
+            // compensate for downwards velocity so jump is always equally high
+            float lastNegativeVelocity = 0;
+            if (rigidBody.GetRelativePointVelocity(Vector2.zero).y < 0)
             {
-                float lastNegativeVelocity = 0;
-                if (rigidBody.GetRelativePointVelocity(Vector2.zero).y < 0)
+                lastNegativeVelocity = -rigidBody.GetRelativePointVelocity(Vector2.zero).y;
+            }
+            rigidBody.AddForce(Vector3.up * (jumpHeight + lastNegativeVelocity), ForceMode2D.Impulse);
+            lastNegativeVelocity = 0;
+
+            // increment jump count if double jump is enabled and reset if it is higher than 1 so you can only jump in the air once 
+            if (doubleJump)
+            {
+                if (jumpCount > 1 || poligonCollider.IsTouching(lastCollision.collider) && lastCollision.collider.tag == "floor" && feetHeight >= FloorHeight)
                 {
-                    lastNegativeVelocity = -rigidBody.GetRelativePointVelocity(Vector2.zero).y;
+                    jumpCount = 0;
                 }
-                rigidBody.AddForce(Vector3.up * (jumpHeight + lastNegativeVelocity), ForceMode2D.Impulse);
-                lastNegativeVelocity = 0;
-                // increment jump count if double jump is enabled and reset if it is higher than 1 so you can only jump in the air once 
-                if (doubleJump)
-                {
-                    if (jumpCount > 1 || capsuleCollider.IsTouching(lastCollision.collider) && lastCollision.collider.tag == "floor" && feetHeight >= FloorHeight)
-                    {
-                        jumpCount = 0;
-                    }
-                    jumpCount++;
-                }
+                jumpCount++;
             }
         }
     }
